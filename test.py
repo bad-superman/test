6aadcb54dc26c8d0ffebc824ec287e18692ae8e1
bad-superman <xiangchao0105@163.com>
2017-11-13 18:00:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import time
 print 'hello jenkins heheh'
 with open('text.log','w') as fw:
   fw.write('hello jenkins')
-for i in range(1000):
+for i in range(10):
   print i
-  time.sleep(2)
+  time.sleep(1)
 raise Exception("test")
