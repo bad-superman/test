9126e67571eef363213dbfd133b6b12e062dcc2b
bad-superman <xiangchao0105@163.com>
2017-11-13 17:41:26 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 #encoding=utf-8
+import time
 print 'hello jenkins heheh'
 with open('text.log','w') as fw:
   fw.write('hello jenkins')
+for i in range(1000):
+  print i
+  time.sleep(2)
+raise Exception("test")
